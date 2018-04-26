503e4c8532e6308bd78053ce4c93c9722954add5
Kay Mok <kaymok3@gmail.com>
2018-04-26 16:46:34 -0400

    Add username to users table

--- models/post.rb
+++ models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings
+
+  validates :quote, :author, presence: true
 end
--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_many :posts
+
+  validates :first_name, :username, :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { in: 6..20 }
 end
