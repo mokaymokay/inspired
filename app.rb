0e55e34153a92bd976de412630b7184c8009841e
Kay Mok <kaymok3@gmail.com>
2018-04-27 09:26:07 -0400

    Add user login page

--- app.rb
+++ app.rb
@@ -6,3 +6,23 @@ require_relative './models/tag'
 require_relative './models/tagging'
 
 set :database, {adapter: 'postgresql', database: 'rumblr'}
+
+get '/' do
+  erb :index
+end
+
+get '/login' do
+  erb :'/users/login'
+end
+
+post '/login' do
+  @user = User.find_by(email: params[:email], password: params[:password])
+  if @user != nil
+    session[:id] = @user.id
+    # TODO: redirect to logged in version of homepage - specify in index.erb
+    redirect '/'
+  else
+    # TODO: display error message instead of redirecting?
+    redirect '/login'
+  end
+end
