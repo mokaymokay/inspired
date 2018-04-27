fb3c787aff6a17905195831beea0770f577c78c1
Kay Mok <kaymok3@gmail.com>
2018-04-27 15:14:36 -0400

    Remove server-side validations (for now) so signup form data gets recorded

--- models/post.rb
+++ models/post.rb
@@ -1,6 +1,4 @@
 class Post < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings
-
-  validates :quote, :author, presence: true
 end
--- models/user.rb
+++ models/user.rb
@@ -1,7 +1,3 @@
 class User < ActiveRecord::Base
   has_many :posts
-
-  validates :first_name, :username, :email, presence: true
-  validates :email, uniqueness: true
-  validates :password, length: { in: 6..20 }
 end
