2d9762bb413f182dada3ad94e3dbb9cb8233b5c5
Kay Mok <kaymok3@gmail.com>
2018-04-30 08:42:11 -0400

    Reseed database and some minor styling changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'faker'
 User.create(first_name: "Kay", last_name: "Mok", email: "kay@example.com", password: "password", birthday: Time.at(rand * Time.now.to_i), username: "mokaymokay")
 User.create(first_name: "Chris", last_name: "Holt", email: "cwh@example.com", password: "password", birthday: Time.at(rand * Time.now.to_i), username: "cwholt")
 
-number_of_users = 5
+number_of_users = 12
 
 (number_of_users - 2).times do
   first = Faker::Name.first_name
@@ -14,7 +14,9 @@ end
 Post.create(quote: "there is nothing to explain: just be, and live.", author: "manzoni", user_id: 1)
 Post.create(quote: "the only constant in a world of tremendous change is the swift passage of time.", author: "cixin liu", user_id: 2)
 
-(number_of_users * 20).times do
+number_of_posts = number_of_users * 50
+
+number_of_posts.times do
   Post.create(quote: Faker::Lorem.sentence, author: Faker::Name.name_with_middle, user_id: 1 + rand(number_of_users))
 end
 
@@ -26,14 +28,16 @@ Tag.create(content: "loss")
 Tag.create(content: "change")
 Tag.create(content: "courage")
 
+number_of_tags = 7
+
 Tagging.create(post_id: 1, tag_id: 1)
 Tagging.create(post_id: 1, tag_id: 2)
 Tagging.create(post_id: 2, tag_id: 1)
 Tagging.create(post_id: 2, tag_id: 3)
 
-150.times do
-  random_post_id = 1 + rand(number_of_users * 20)
-  random_tag_id = 1 + rand(7)
+(number_of_posts * 3).times do
+  random_post_id = 1 + rand(number_of_posts)
+  random_tag_id = 1 + rand(number_of_tags)
   if Tagging.find_by(post_id: random_post_id, tag_id: random_tag_id) == nil
     Tagging.create(post_id: random_post_id, tag_id: random_tag_id)
   end
